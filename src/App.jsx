d8f1958f48c1c7f71c0e1a7e26fd12eda434eea7
Biju Borah <biju20_ug@ee.nits.ac.in>
2024-05-05 18:22:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { io } from 'socket.io-client'
-import { LineChart, XAxis, Tooltip, CartesianGrid, Line } from 'recharts'
+import { LineChart, XAxis, Tooltip, CartesianGrid, Line, YAxis } from 'recharts'
 
 const App = () => {
   const [data, setData] = React.useState([])
@@ -17,12 +17,13 @@ const App = () => {
 
   }, [])
   return (
-    <LineChart width={1000} height={400} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+    <LineChart width={1200} height={480} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
       <XAxis dataKey="name" />
       <Tooltip />
       <CartesianGrid stroke="#f5f5f5" />
       <Line type="monotone" dataKey="x" stroke="#ff7300" yAxisId={0} />
-      <Line type="monotone" dataKey="y" stroke="#387908" yAxisId={1} />
+      {/* <Line type="monotone" dataKey="y" stroke="#387908" yAxisId={1} /> */}
+      {/* <YAxis type="number" domain={[-1, 1]} /> */}
     </LineChart>
   )
 }
